24017f98bb85a63b164d57bb08ae36c7727070d9
RANJITHp7 <105112146+RANJITHp7@users.noreply.github.com>
2022-05-15 11:25:59 +0530

    Add files via upload

---
+++ sum of digits.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+sum(int n){ // function used to find the sum
+	if(n==0)
+	   return 0;
+	else      
+	    return n%10+sum(n/10);
+}
+int main(){
+	int i;
+	printf("enter the number:");
+	scanf("%d",&i);
+	printf("Sum of digits is %d",sum(i)); 
+}
